4914b92b6e1fbd116f55c7c1a05278db8eadb7b0
vihan <vihanvimen46@gamail.com>
2025-07-23 09:42:43 +0530

    Initial commit

--- src/main/java/com/example/layeredarchitecture/dao/CrudDAO.java
+++ src/main/java/com/example/layeredarchitecture/dao/CrudDAO.java
@@ -3,7 +3,18 @@ package com.example.layeredarchitecture.dao;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+public interface CrudDAO<T> extends SuperDAO {
+    public ArrayList<T> loadAll() throws SQLException, ClassNotFoundException;
+
+    public boolean save(T dto) throws SQLException, ClassNotFoundException;
+
+    public boolean update(T dto) throws SQLException, ClassNotFoundException;
+
     public boolean delete(String id) throws SQLException, ClassNotFoundException;
+
+    public boolean exists(String id) throws SQLException, ClassNotFoundException;
+
     public String generateNewId() throws SQLException, ClassNotFoundException;
+
     public T search(String newValue) throws SQLException, ClassNotFoundException;
 }
